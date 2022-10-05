82e4f0f19bd99f609b83524f11afcf7fe57ca9fb
gorka etxebarria <gorka.etxebarria@gmail.com>
2022-10-05 11:21:20 +0200

    divider multiplyer

--- Js course/MultiplyDivide/index.js
+++ Js course/MultiplyDivide/index.js
@@ -1,13 +1,27 @@
-function multiplyBy() {
-  console.log("multiply");
-  num1 = document.getElementById("firstNumber").value;
-  num2 = document.getElementById("secondNumber").value;
-  document.getElementById("result").innerHTML = num1 * num2;
-}
+window.onload = () => {
+  console.log(document.getElementsByClassName("multiply"));
+  const multiplybutton = document.getElementsByClassName("multiply");
+  multiplybutton.addEventListener(
+    "click",
+    (multiplyBy = () => {
+      console.log("multiply");
+      num1 = document.getElementById("firstNumber").value;
+      num2 = document.getElementById("secondNumber").value;
+      document.getElementById("result").innerHTML = num1 * num2;
+    })
+  );
 
-function divideBy() {
-  console.log("divide");    
-  num1 = document.getElementById("firstNumber").value;
-  num2 = document.getElementById("secondNumber").value;
-  document.getElementById("result").innerHTML = num1 / num2;
-}
+  console.log(document.getElementsByClassName("divide"));
+  const dividebutton = document.getElementsByClassName("divide");
+  dividebutton.addEventListener(
+    "click",
+    (divideBy = () => {
+      console.log("multiply");
+      onsole.log("divide");
+      num1 = document.getElementById("firstNumber").value;
+      num2 = document.getElementById("secondNumber").value;
+      document.getElementById("result").innerHTML = num1 / num2;
+    })
+  );
+};
+
