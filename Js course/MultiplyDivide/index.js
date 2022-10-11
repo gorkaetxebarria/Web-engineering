2f6b2dc98fd4ed4366bfc4e7b9657df93a62436e
gorka etxebarria <gorka.etxebarria@gmail.com>
2022-10-11 10:04:28 +0200

    comit

--- Js course/MultiplyDivide/index.js
+++ Js course/MultiplyDivide/index.js
@@ -3,25 +3,24 @@ window.onload = () => {
   const multiplybutton = document.getElementsByClassName("multiply");
   multiplybutton.addEventListener(
     "click",
-    (multiplyBy = () => {
+    (multiplyBy) = () => {
       console.log("multiply");
       num1 = document.getElementById("firstNumber").value;
       num2 = document.getElementById("secondNumber").value;
       document.getElementById("result").innerHTML = num1 * num2;
-    })
+    }
   );
 
   console.log(document.getElementsByClassName("divide"));
   const dividebutton = document.getElementsByClassName("divide");
   dividebutton.addEventListener(
     "click",
-    (divideBy = () => {
-      console.log("multiply");
+    (divideBy) = () => {
       onsole.log("divide");
       num1 = document.getElementById("firstNumber").value;
       num2 = document.getElementById("secondNumber").value;
       document.getElementById("result").innerHTML = num1 / num2;
-    })
+    }
   );
 };
 
