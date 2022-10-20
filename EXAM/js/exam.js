ce7ac57632386e4a4b84cad3e4ec87214299999b
gorka etxebarria <gorka.etxebarria@gmail.com>
2022-10-20 13:24:09 +0200

    exam commit

---
+++ EXAM/js/exam.js
@@ -0,0 +1,30 @@
+const time = 3000;
+var unselectTimeout;
+var hideTimeout;
+
+window.onload = function(){
+  document.querySelectorAll("subject").forEach(function(element){
+    element.addEventListener("click", subjectClick);
+  })
+  document.getElementById("hide").addEventListener("click", hide);
+  document.getElementById("cancel").addEventListener("click", cancel);
+}
+
+function subjectClick(e){
+  e.currentTarget.classList.toggle("selected");
+}
+
+
+function hide(e){
+  clearTimeout(hideTimeout);
+  hideTimeout = setTimeout(function(){
+    document.querySelectorAll(".selected").forEach(function(element){
+      element.classList.add("hidden");
+    });
+  }, time);
+}
+
+function cancel(e){
+  clearTimeout(unselectTimeout);
+  clearTimeout(hideTimeout); 
+}
